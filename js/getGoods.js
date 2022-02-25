3e81247f35b36706d2147d2faae9b17a1395545d
raptorVladimirAdigeya <98263225+raptorVladimirAdigeya@users.noreply.github.com>
2022-02-25 15:26:41 +0300

    Add files via upload

---
+++ js/getGoods.js
@@ -0,0 +1,69 @@
+const getGoods = () => {
+    const links = document.querySelectorAll('.navigation-link');
+
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list');
+
+        //обнуляем карточки
+        goodsContainer.innerHTML = "";
+        
+        goods.forEach(good  => {
+            const goodBlock = document.createElement('div');
+
+            
+
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-6');
+            //отрисовываем карточки
+            goodBlock.innerHTML = `
+            <div class="goods-card">
+                <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                <h3 class="goods-title">${good.name}</h3>                
+                <p class="goods-description">${good.description}</p>                                
+                <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                    <span class="button-price">$${good.price}</span>
+                </button>
+            </div>
+            `;
+            goodsContainer.append(goodBlock);
+            
+        })
+    }
+    
+    const getData = (value, category) => {
+        fetch('https://test-bbea7-default-rtdb.firebaseio.com/db.json')
+        .then((res) =>  res.json())
+        .then((data) => {
+            //фильтруем массив            
+            const array = category ? data.filter((item) =>  item[category] === value ) : data
+             //созраняем в localStarage           
+            localStorage.setItem('goods', JSON.stringify(array));
+
+            if (window.location.pathname !== "/goods.html") {
+                window.location.href = '/goods.html';
+            } else {
+                renderGoods(array);
+            }
+            
+        })
+    }
+
+    links.forEach((link) => {
+       
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const linkValue = link.textContent;
+            const category = link.dataset.field; 
+            
+
+            getData(linkValue, category);
+            
+        })
+    })    
+    if (localStorage.getItem('goods') && (window.location.pathname === "/goods.html")) {
+        renderGoods(JSON.parse(localStorage.getItem('goods')));
+    }
+}
+
+getGoods();
--- js/search.js
+++ js/search.js
@@ -1,10 +1,63 @@
 const search = function() {
     const input = document.querySelector('.search-block>input');
     const searchBtn = document.querySelector('.search-block>button');
-   
-    searchBtn.addEventListener('click', (event) => {
-        console.log(input.value)
-    })    
+    //создаем карточки
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list');
+
+        //обнуляем карточки
+        goodsContainer.innerHTML = "";
+        
+        goods.forEach(good  => {
+            const goodBlock = document.createElement('div');
+
+            
+
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-6');
+            //отрисовываем карточки
+            goodBlock.innerHTML = `
+            <div class="goods-card">
+                <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                <h3 class="goods-title">${good.name}</h3>                
+                <p class="goods-description">${good.description}</p>                                
+                <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                    <span class="button-price">$${good.price}</span>
+                </button>
+            </div>
+            `;
+            goodsContainer.append(goodBlock);
+            
+        })
+    }
+    
+    const getData = (value) => {
+        fetch('https://test-bbea7-default-rtdb.firebaseio.com/db.json')
+        .then((res) =>  res.json())
+        .then((data) => {
+            //фильтруем массив            
+            const array = data.filter(good => {
+                return good.name.toLowerCase().includes(value.toLowerCase())
+            })
+            
+             //созраняем в localStarage           
+            localStorage.setItem('goods', JSON.stringify(array));
+
+            if (window.location.pathname !== "/goods.html") {
+                window.location.href = '/goods.html';
+            } else {
+                renderGoods(array);
+            }
+            
+        })
+    }
+
+   //поиск по кнопки
+    searchBtn.addEventListener('click', () => {
+        
+        getData(input.value);
+    }) 
 }
 
 search();
