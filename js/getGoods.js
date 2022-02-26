c94f6b21d079d23a64cef09255a1cc4666912b1d
raptorVladimirAdigeya <98263225+raptorVladimirAdigeya@users.noreply.github.com>
2022-02-26 20:59:09 +0300

    Add files via upload

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,5 +1,6 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link');
+    const more = document.querySelector('.more');
 
     const renderGoods = (goods) => {
         const goodsContainer = document.querySelector('.long-goods-list');
@@ -63,6 +64,14 @@ const getGoods = () => {
     })    
     if (localStorage.getItem('goods') && (window.location.pathname === "/goods.html")) {
         renderGoods(JSON.parse(localStorage.getItem('goods')));
+    }
+    if (more) {
+        more.addEventListener('click', (event) => {
+            event.preventDefault();
+              
+
+            getData();
+        })
     }
 }
 
